8332cf0265c8d790997c1c199b207edbe21efe61
jasonmlutz <jason.m.lutz@gmail.com>
2019-01-16 13:34:57 -0800

    include the proper api token call

--- input_verification.py
+++ input_verification.py
@@ -4,6 +4,7 @@
 from mapbox import Geocoder
 from Credentials import mapbox_token
 def verify_location(user_input):
+    geocoder = Geocoder(access_token=str(mapbox_token))
     response = geocoder.forward(str(user_input))
     collection = response.json()
     n = len(collection['features'])
