9d340d4f6c133851e0dbb696d9ca506f2b2e0cfd
jasonmlutz <jason.m.lutz@gmail.com>
2019-01-16 14:03:58 -0800

    Added docstring, improved formatting

--- input_verification.py
+++ input_verification.py
@@ -3,13 +3,28 @@
     is then returned to the user for verification."""
 from mapbox import Geocoder
 from Credentials import mapbox_token
-def verify_location(user_input):
+def location_candidates(user_input):
+    """
+    Returns a dictionary of possible locations.
+
+    With the goal of correctly interpreting a user's input location input, this
+    function utilizes the Geocoder feature of Mapbox to return a dictionary of
+    possible candidates for the users intended input locationself.
+
+    Parameters:
+    user_input (str): A location, such as street address or landmark.
+
+    Returns:
+    candidates (dict): A dictionary encoding the responses from Mapbox based on
+    the user's input. Keys are integers 0, 1, ...; values are the precise place
+    names for the fetched locations.
+    """
     geocoder = Geocoder(access_token=str(mapbox_token))
     response = geocoder.forward(str(user_input))
     collection = response.json()
-    n = len(collection['features'])
-    location_candidates = {}
-    keys = range(n)
+    num_features = len(collection['features'])
+    candidates = {}
+    keys = range(num_features)
     for i in keys:
-        location_candidates[i] = collection['features'][i]['place_name']
-    return(location_candidates)
+        candidates[i] = collection['features'][i]['place_name']
+    return candidates
