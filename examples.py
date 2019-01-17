9e96c90b422d60fc2dd43ecb60a29a0a71741673
jasonmlutz <jason.m.lutz@gmail.com>
2019-01-16 19:56:25 -0800

    More examples to test the .geojson() method

--- examples.py
+++ examples.py
@@ -1,7 +1,13 @@
 """ Testing ground for route-weather fuctionallity.
     """
+#import json
 from mapbox import Geocoder
 from Credentials import mapbox_token
 from input_verification import location_candidates, display_and_verify
 d = location_candidates('213 N Main, CA')
 display_and_verify(d)
+#origin = '416 Sid Snyder Ave SW, Olympia, WA 98504'
+#origin = 'Main St, CA'
+#geocoder = geocoder = Geocoder(access_token=str(mapbox_token))
+#response = geocoder.forward(origin, limit = 20)
+#len(response.geojson()['features']) #in practice, limit <= 10 ?
