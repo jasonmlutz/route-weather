289283d31138d90d9854b0bc231229b526e872ab
jasonmlutz <jason.m.lutz@gmail.com>
2019-01-16 16:38:01 -0800

    Added examples for testing

---
+++ examples.py
@@ -0,0 +1,7 @@
+""" Testing ground for route-weather fuctionallity.
+    """
+from mapbox import Geocoder
+from Credentials import mapbox_token
+from input_verification import location_candidates, display_and_verify
+d = location_candidates('213 N Main, CA')
+display_and_verify(d)
