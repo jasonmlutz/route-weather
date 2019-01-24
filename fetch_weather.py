c2918132f96d4c1bb2d91e5b1f271934b7711506
Jason Lutz <jasonlutz@cilantro.local>
2019-01-23 22:22:50 -0800

    initial code for testing Mapbox/Dark Sky compatibility

---
+++ fetch_weather.py
@@ -0,0 +1,13 @@
+"""Methods for fetching weather data from Darksky api. The current goal is to
+    make sure that the output from Mapbox can be made compatible with the
+    inputs to the DarkSky api.
+    """
+from darksky import forecast
+from mapbox import Geocoder
+from Credentials import mapbox_token, darksky_token
+origin = '416 Sid Snyder Ave SW, Olympia, WA 98504'
+geocoder = Geocoder(access_token=str(mapbox_token))
+response_origin = geocoder.forward(str(origin))
+coords = response_origin.json()['features'][0]['center']
+olympia = forecast(darksky_token, coords[1], coords[0])
+a = olympia['currently']
