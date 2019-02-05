24fc3edf73f27eb3e73d637b4a8a1d81ce1131f9
Jason Lutz <jasonlutz@cilantro.local>
2019-02-04 22:42:42 -0800

    minor changes

--- scratch.py
+++ scratch.py
@@ -5,20 +5,20 @@ from fetch_weather import fetch_weather_summary
 from fetch_directions import *
 from fetch_departure_time import *
 origin = '416 Sid Snyder Ave SW, Olympia, WA 98504'
-#destination = '1315 10th St Room B-27, Sacramento, CA 95814'
+destination = '1315 10th St Room B-27, Sacramento, CA 95814'
 geocoder = Geocoder(access_token=str(mapbox_token))
-#service = Directions(access_token=str(mapbox_token))
+service = Directions(access_token=str(mapbox_token))
 response_origin = geocoder.forward(str(origin))
 origin_proper = response_origin.json()['features'][0]
-#response_destination = geocoder.forward(str(destination))
-#destination_proper = response_destination.json()['features'][0]
-#response = service.directions([origin_proper, destination_proper], profile='mapbox/driving', steps=True)
+response_destination = geocoder.forward(str(destination))
+destination_proper = response_destination.json()['features'][0]
+response = service.directions([origin_proper, destination_proper], profile='mapbox/driving', steps=True)
 #a = fetch_directions_summary(origin_proper, destination_proper)
 #print("\n".join("{}: {}, {}, {}".format(k, v[0], v[1], v[2]) for k, v in a.items()))
 
-t = get_departure_time()
-coords = list(reversed(origin_proper['center']))
-wx = fetch_weather_summary(coords[0], coords[1], t)
+#t = get_departure_time()
+#coords = list(reversed(origin_proper['center']))
+#wx = fetch_weather_summary(coords[0], coords[1], t)
 
 
 
